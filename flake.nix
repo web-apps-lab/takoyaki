cac42e52299278e35b5f999401672bc30d671d68
Fabien Boucher <web.apps.top@gmail.com>
2023-02-17 16:29:19 +0000

    Attempting to integrate with butler

--- flake.nix
+++ flake.nix
@@ -11,45 +11,31 @@
   outputs = { self, hspkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
-        packageName = "takoyaki";
         pkgs = hspkgs.pkgs;
-        haskellPackages = pkgs.hspkgs;
-        myPackage = haskellPackages.callCabal2nix packageName self { };
-        takoyakiExe = pkgs.haskell.lib.justStaticExecutables myPackage;
 
-        containerHome = "var/lib/${packageName}";
-        mkContainerHome = "mkdir -p -m 744 ${containerHome}";
+        packageName = "takoyaki";
 
-        takoyakiContainer = pkgs.dockerTools.buildLayeredImage {
-          name = "localhost/takoyaki";
-          contents = [ pkgs.coreutils pkgs.bash takoyakiExe ];
-          extraCommands = "${mkContainerHome}";
-          tag = "latest";
-          created = "now";
-          config = {
-            Env = [
-              "HOME=/${containerHome}"
-              # Use fakeroot to avoid `No user exists for uid` error
-              "LD_PRELOAD=${pkgs.fakeroot}/lib/libfakeroot.so"
-            ];
-          };
+        haskellExtend = hpFinal: hpPrev: {
+          takoyaki = hpPrev.callCabal2nix packageName self { };
         };
+
+        hsPkgs = pkgs.hspkgs.extend haskellExtend;
+
+        takoyakiExe = pkgs.haskell.lib.justStaticExecutables hsPkgs.takoyaki;
 
       in {
         defaultExe = takoyakiExe;
-        defaultPackage = myPackage;
+        defaultPackage = hsPkgs.takoyaki;
 
-        packages.container = takoyakiContainer;
+        devShell = hsPkgs.shellFor {
+          packages = p: [ p.takoyaki ];
 
-        devShell = haskellPackages.shellFor {
-          packages = p: [ myPackage ];
-
-          buildInputs = [
-            pkgs.ghcid
-            pkgs.ormolu
-            pkgs.cabal-install
-            pkgs.hlint
-            pkgs.haskell-language-server
+          buildInputs = with pkgs; [
+            ghcid
+            ormolu
+            cabal-install
+            hlint
+            haskell-language-server
           ];
         };
       });
